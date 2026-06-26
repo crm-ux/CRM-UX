93f36900ba505703cf49b870a94d4574f59ed09b
Ultrawin CRM <crm@ultrawin.in>
2026-06-26 13:58:56 +0000

    Fix: always set all terms in create

--- addons/custom_crm_extended/wizard/sale_quote_preview_wizard.py
+++ addons/custom_crm_extended/wizard/sale_quote_preview_wizard.py
@@ -82,8 +82,7 @@ class SaleQuotePreviewWizard(models.TransientModel):
         records = super().create(vals_list)
         all_terms = self.env['sale.terms.condition'].search([], order='sequence, id')
         for rec in records:
-            if not rec.selected_term_ids:
-                rec.selected_term_ids = [(6, 0, all_terms.ids)]
+            rec.selected_term_ids = [(6, 0, all_terms.ids)]
         return records
 
     @api.onchange('order_id')
