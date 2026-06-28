442f265e5b92bd18e02b71c5622b637cfe3a4f9a
Ultrawin CRM <crm@ultrawin.in>
2026-06-28 11:04:36 +0000

    Fix: inject CSS in both header and body to remove underlines

--- addons/custom_crm_extended/models/patch_report.py
+++ addons/custom_crm_extended/models/patch_report.py
@@ -5,9 +5,9 @@ class IrActionsReportPatch(models.Model):
 
     def _prepare_html(self, html, report_model=False):
         bodies, html_ids, header, footer, specific_paperformat_args = super()._prepare_html(html, report_model=report_model)
+        fix_css = '<style>a,a:link,a:visited,a:hover,a:active{text-decoration:none!important;border:none!important;border-bottom:none!important;color:#000!important;}header,div.header,.header{border:none!important;border-top:none!important;border-bottom:none!important;box-shadow:none!important;}*{text-decoration:none!important;}</style>'
         if header:
-            header = header.replace(
-                '</head>',
-                '<style>header,div.header,.header{border:none!important;border-top:none!important;border-bottom:none!important;box-shadow:none!important;outline:none!important;}a,a:link,a:visited,a:hover{text-decoration:none!important;border:none!important;color:#000!important;}*{border-top-color:#fff!important;border-bottom-color:#fff!important;}</style></head>'
-            )
+            header = header.replace('</head>', fix_css + '</head>')
+        if bodies:
+            bodies = [b.replace('</head>', fix_css + '</head>') if '</head>' in b else b for b in bodies]
         return bodies, html_ids, header, footer, specific_paperformat_args
