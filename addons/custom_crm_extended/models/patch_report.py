a751c1c476254f8ac8f04bd07fd11c09a1d4a953
Ultrawin CRM <crm@ultrawin.in>
2026-06-28 09:19:52 +0000

    Fix: _prepare_html returns tuple not dict

--- addons/custom_crm_extended/models/patch_report.py
+++ addons/custom_crm_extended/models/patch_report.py
@@ -4,11 +4,10 @@ class IrActionsReportPatch(models.Model):
     _inherit = 'ir.actions.report'
 
     def _prepare_html(self, html, report_model=False):
-        result = super()._prepare_html(html, report_model=report_model)
-        # Remove red border line from header
-        if result and result.get('header'):
-            result['header'] = result['header'].replace(
+        bodies, html_ids, header, footer, specific_paperformat_args = super()._prepare_html(html, report_model=report_model)
+        if header:
+            header = header.replace(
                 '</head>',
-                '<style>header,div.header,.header{border:none!important;border-top:none!important;border-bottom:none!important;box-shadow:none!important;outline:none!important;}*{border-top-color:transparent!important;}</style></head>'
+                '<style>header,div.header,.header{border:none!important;border-top:none!important;border-bottom:none!important;box-shadow:none!important;}</style></head>'
             )
-        return result
+        return bodies, html_ids, header, footer, specific_paperformat_args
