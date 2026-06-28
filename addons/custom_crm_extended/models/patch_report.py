373f8ce862d2515eff6c0a4a9265cab0f241de77
Ultrawin CRM <crm@ultrawin.in>
2026-06-28 09:07:25 +0000

    Fix: remove red line via header CSS patch, logo on all pages

--- addons/custom_crm_extended/models/patch_report.py
+++ addons/custom_crm_extended/models/patch_report.py
@@ -5,9 +5,12 @@ class IrActionsReportPatch(models.Model):
 
     def _run_wkhtmltopdf(self, bodies, report_ref=False, header=None, footer=None, landscape=False, specific_paperformat_args=None, set_viewport_size=False):
         if header:
-            # Inject CSS to remove border lines from header
             header = header.replace(
                 '</head>',
-                '<style>header,.header,div.header{border:none !important;border-top:none !important;border-bottom:none !important;border-left:none !important;border-right:none !important;box-shadow:none !important;}th,td{border-top:none !important;}</style></head>'
+                '<style>header,.header,div.header{border:none !important;border-top:none !important;border-bottom:none !important;box-shadow:none !important;}</style></head>'
             )
-        return super()._run_wkhtmltopdf(bodies, report_ref=report_ref, header=header, footer=footer, landscape=landscape, specific_paperformat_args=specific_paperformat_args, set_viewport_size=set_viewport_size)
+        return super()._run_wkhtmltopdf(
+            bodies, report_ref=report_ref, header=header, footer=footer,
+            landscape=landscape, specific_paperformat_args=specific_paperformat_args,
+            set_viewport_size=set_viewport_size
+        )
