368f998d9724a64ad625575fce6f3094fea5f3a6
Ultrawin CRM <crm@ultrawin.in>
2026-06-28 09:27:42 +0000

    Fix: remove link underlines from PDF header on all pages

--- addons/custom_crm_extended/models/patch_report.py
+++ addons/custom_crm_extended/models/patch_report.py
@@ -8,6 +8,6 @@ class IrActionsReportPatch(models.Model):
         if header:
             header = header.replace(
                 '</head>',
-                '<style>header,div.header,.header{border:none!important;border-top:none!important;border-bottom:none!important;box-shadow:none!important;}</style></head>'
+                '<style>header,div.header,.header{border:none!important;border-top:none!important;border-bottom:none!important;box-shadow:none!important;}a,a:link,a:visited,a:hover{text-decoration:none!important;border:none!important;color:#000!important;}</style></head>'
             )
         return bodies, html_ids, header, footer, specific_paperformat_args
