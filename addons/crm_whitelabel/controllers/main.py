4c378d9419b38e3fffaa2aa1ca90b4d1a9eee447
crm-ux <crm@yantradynamics.in>
2026-08-26 11:15:32 +0530

    Update code for testing

--- addons/crm_whitelabel/controllers/main.py
+++ addons/crm_whitelabel/controllers/main.py
@@ -1,8 +1,23 @@
 from odoo import http
 from odoo.http import request
+odoo.addons.web.controllers.session import Session
 
 class CrmWhitelabelController(http.Controller):
     
     @http.route(['/favicon.ico'], type='http', auth='public', website=True, multilang=False, sitemap=False, readonly=True)
     def favicon(self, **kw):
         return request.redirect('/web/static/img/favicon.ico', code=301)
+    
+class PersistentSession(Session):
+    @http.route('/web/session/authenticate', type='json', auth="none")
+    def authenticate(self, db, login, password, base_location=None):
+        res = super(PersistentSession, self).authenticate(db, login, password, base_location)
+        if request and request.session and request.session.sid:
+            request.future_response.set_cookie(
+                'session_id',
+                request.session.sid,
+                max_age=90 * 24 * 60 * 60,
+                httponly=True
+            )
+        return res
+
