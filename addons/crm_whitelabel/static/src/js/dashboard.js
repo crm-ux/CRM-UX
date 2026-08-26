a0219854884c4142d6304527c423f06293ce57e8
crm-ux <crm@yantradynamics.in>
2026-08-26 16:16:44 +0530

    Update code for testing

--- addons/crm_whitelabel/static/src/js/dashboard.js
+++ addons/crm_whitelabel/static/src/js/dashboard.js
@@ -223,21 +223,32 @@ class CrmDashboard extends Component {
     }
 
     async openEquipmentMaster() {
-        const wizardId = await this.ormService.create("equipment.master.wizard", [{ name: "New Equipment", step: 1 }]);
-        const viewId = await rpc("/web/dataset/call_kw", {
-            model: "ir.model.data",
-            method: "check_object_reference",
-            args: ["custom_crm_extended", "equipment_master_wizard_form"],
-            kwargs: {},
-        });
-        this.go({
-            type: "ir.actions.act_window",
-            name: "Equipment Master Creation",
-            res_model: "equipment.master.wizard",
-            res_id: wizardId[0],
-            views: [[viewId[1], "form"]],
-            target: "new",
-        });
+        try {
+            const wizardId = await this.ormService.create("equipment.master.wizard", [{ name: "New Equipment", step: 1 }]);
+            let viewId = false;
+            try {
+                const res = await this.ormService.searchRead(
+                    "ir.model.data",
+                    [["module", "=", "custom_crm_extended"], ["name", "=", "equipment_master_wizard_form"]],
+                    ["res_id"]
+                );
+                if (res && res.length > 0) {
+                    viewId = res[0].res_id;
+                }
+            } catch (e) { }
+
+            const action = {
+                type: "ir.actions.act_window",
+                name: "Equipment Master Creation",
+                res_model: "equipment.master.wizard",
+                res_id: wizardId[0],
+                views: viewId ? [[viewId, "form"]] : [[false, "form"]],
+                target: "new",
+            };
+            this.go(action);
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     async newLead() {
