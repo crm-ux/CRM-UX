f7fafb53725a0117befe0d037a757f13c6ad3f83
crm-ux <crm@yantradynamics.in>
2026-08-26 11:23:25 +0530

    Update code for testing

--- addons/crm_whitelabel/controllers/main.py
+++ addons/crm_whitelabel/controllers/main.py
@@ -1,6 +1,6 @@
 from odoo import http
 from odoo.http import request
-from odoo.addons.web.controllers.session import Session
+from odoo.addons.web.controllers.home import Home
 
 class CrmWhitelabelController(http.Controller):
     
@@ -8,16 +8,13 @@ class CrmWhitelabelController(http.Controller):
     def favicon(self, **kw):
         return request.redirect('/web/static/img/favicon.ico', code=301)
     
-class PersistentSession(Session):
-    @http.route('/web/session/authenticate', type='json', auth="none")
-    def authenticate(self, db, login, password, base_location=None):
-        res = super(PersistentSession, self).authenticate(db, login, password, base_location)
-        if request and request.session and request.session.sid:
-            request.future_response.set_cookie(
-                'session_id',
-                request.session.sid,
-                max_age=90 * 24 * 60 * 60,
-                httponly=True
-            )
-        return res
+class PersistentHome(Home):
+    @http.route('/web/login', type='http', auth="none")
+    def web_login(self, redirect=None, **kw):
+        response = super(PersistentHome, self).web_login(redirect=redirect, **kw)
+        if request and request.session and request.session.uid and response:
+            # Set session_id cookie max-age to 90 days (7,776,000 seconds)
+            response.set_cookie('session_id', request.session.sid, max_age=90 * 24 * 60 * 60, httponly=True)
+        return response
+
 
