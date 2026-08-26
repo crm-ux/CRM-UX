0b7df277a04804e445bca856e3791b77fe6c04a3
crm-ux <crm@yantradynamics.in>
2026-08-26 16:53:29 +0530

    Update code for testing

--- addons/custom_crm_extended/__manifest__.py
+++ addons/custom_crm_extended/__manifest__.py
@@ -36,7 +36,7 @@
         'views/product_label_view.xml',
         'views/product_make_view.xml',
         'views/equipment_master_views.xml',
-        'wizard/equipment_master_wizard_views.xml',
+        'views/equipment_master_wizard_views.xml',
         'views/res_users_signature_views.xml',
         'views/res_company_signature_views.xml',
     ],
