812fed278d270128384caf44961edf9d08748926
crm-ux <crm@yantradynamics.in>
2026-08-27 17:35:48 +0530

    Update code for testing

--- addons/custom_crm_extended/static/src/js/equipement_form.js
+++ addons/custom_crm_extended/static/src/js/equipement_form.js
@@ -11,15 +11,14 @@ export class EquipmentFormController extends FormController {
         if (this.props.resModel === "equipment.master") {
             let observer = null;
             const reorderToolbar = () => {
-                const dashboardBtn = document.querySelector(".o_control_panel_breadcrumbs > button.ms-1");
-                const mainButtons = document.querySelector(".o_control_panel_breadcrumbs > .o_control_panel_main_buttons");
-                const statusIndicator = document.querySelector(".o_control_panel_breadcrumbs > .o_form_status_indicator");
+                const container = document.querySelector(".o_control_panel_breadcrumbs");
                 const breadcrumb = document.querySelector(".o_control_panel_breadcrumbs > .o_breadcrumb");
-
-                if (dashboardBtn) dashboardBtn.style.order = "1";
-                if (mainButtons) mainButtons.style.order = "2";
-                if (statusIndicator) statusIndicator.style.order = "3";
-                if (breadcrumb) breadcrumb.style.order = "4";
+                const statusIndicator = document.querySelector(".o_control_panel_breadcrumbs > .o_form_status_indicator");
+                if (container && breadcrumb && statusIndicator) {
+                    if (statusIndicator.nextElementSibling !== breadcrumb) {
+                        container.insertBefore(statusIndicator, breadcrumb);
+                    }
+                }
             };
 
             onMounted(() => {
