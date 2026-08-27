2220133eed7aaac6e33ef10c78b0efb54fcb996c
crm-ux <crm@yantradynamics.in>
2026-08-27 16:59:41 +0530

    Update code for testing

--- addons/custom_crm_extended/static/src/js/equipement_form.js
+++ addons/custom_crm_extended/static/src/js/equipement_form.js
@@ -1,10 +1,41 @@
 /** @odoo-module **/
 
+import { onMounted, onWillUnmount } from "@odoo/owl";
 import { FormController } from "@web/views/form/form_controller";
 import { formView } from "@web/views/form/form_view";
 import { registry } from "@web/core/registry";
 
-export class EquipmentFormController extends FormController { }
+export class EquipmentFormController extends FormController {
+    setup() {
+        super.setup();
+        if (this.props.resModel === "equipment.master") {
+            let observer = null;
+            const reorderToolbar = () => {
+                const container = document.querySelector(".o_control_panel_breadcrumbs");
+                const breadcrumb = document.querySelector(".o_control_panel_breadcrumbs > .o_breadcrumb");
+                const statusIndicator = document.querySelector(".o_control_panel_breadcrumbs > .o_form_status_indicator");
+                if (container && breadcrumb && statusIndicator) {
+                    if (breadcrumb.previousElementSibling !== statusIndicator) {
+                        container.insertBefore(breadcrumb, statusIndicator.nextSibling);
+                    }
+                }
+            };
+
+            onMounted(() => {
+                reorderToolbar();
+                const panel = document.querySelector(".o_control_panel_breadcrumbs");
+                if (panel) {
+                    observer = new MutationObserver(() => reorderToolbar());
+                    observer.observe(panel, { childList: true, subtree: true });
+                }
+            });
+
+            onWillUnmount(() => {
+                if (observer) observer.disconnect();
+            });
+        }
+    }
+}
 
 export const equipmentFormView = {
     ...formView,
