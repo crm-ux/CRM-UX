4527e4282dc8890c5b892c5275e6af238750887f
crm-ux <crm@yantradynamics.in>
2026-09-02 11:43:24 +0530

    Update code for testing

--- addons/crm_whitelabel/controllers/main.py
+++ addons/crm_whitelabel/controllers/main.py
@@ -1,33 +1,16 @@
 from odoo import http
-from odoo.http import request, root, SessionExpiredException
+from odoo.http import request
 from odoo.addons.web.controllers.home import Home
 
-# 90 Days in Seconds
-# 1 Year in Seconds (365 Days)
 SESSION_1_YEAR = 365 * 24 * 60 * 60
-
-# Override Server-side Session Lifetime on Disk
-if hasattr(root, 'session_store'):
-    root.session_store.session_timeout = SESSION_1_YEAR
-
-# Patch FutureResponse.set_cookie to enforce 1 year on all session_id cookies
-orig_set_cookie = http.FutureResponse.set_cookie
-
-def persistent_set_cookie(self, key, value='', max_age=None, expires=None, path='/', domain=None, secure=False, httponly=False, samesite=None, cookie_type='required'):
-    if key == 'session_id':
-        max_age = SESSION_1_YEAR
-        expires = None
-    return orig_set_cookie(self, key, value=value, max_age=max_age, expires=expires, path=path, domain=domain, secure=secure, httponly=httponly, samesite=samesite, cookie_type=cookie_type)
-
-http.FutureResponse.set_cookie = persistent_set_cookie
 
 
 class CrmWhitelabelController(http.Controller):
-    
     @http.route(['/favicon.ico'], type='http', auth='public', website=True, multilang=False, sitemap=False, readonly=True)
     def favicon(self, **kw):
         return request.redirect('/web/static/img/favicon.ico', code=301)
-    
+
+
 class PersistentHome(Home):
     @http.route('/', type='http', auth="none")
     def index(self, s_action=None, **kw):
@@ -41,8 +24,11 @@ class PersistentHome(Home):
             return request.redirect('/app/action-435')
         
         response = super(PersistentHome, self).web_login(redirect=redirect, **kw)
-
-        # Enforce 1 year on login response
+        
         if request and request.session and request.session.uid and response:
-            response.set_cookie('session_id', request.session.sid, max_age=SESSION_1_YEAR, httponly=True)
-
+            try:
+                response.set_cookie('session_id', request.session.sid, max_age=SESSION_1_YEAR, httponly=True)
+            except Exception:
+                pass
+                
+        return response
