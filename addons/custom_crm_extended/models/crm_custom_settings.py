f912dfa16adc3a02e554b3e523bc190175bcb54e
crm-ux <crm@yantradynamics.in>
2026-09-03 11:44:18 +0530

    Update code for testing

--- addons/custom_crm_extended/models/crm_custom_settings.py
+++ addons/custom_crm_extended/models/crm_custom_settings.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 
 class CrmCustomSettings(models.TransientModel):
@@ -6,6 +6,13 @@ class CrmCustomSettings(models.TransientModel):
     _description = 'CRM Custom System Settings'
 
     name = fields.Char(default='CRM System Settings')
+
+    # Equipment Master Auto-Number Settings
+    equipment_id_prefix = fields.Char(string='Equipment ID Prefix', default='EQ')
+    equipment_id_auto = fields.Boolean(string='Auto-generate Equipment ID', default=True)
+
+    serial_number_prefix = fields.Char(string='Serial Number Prefix', default='SN')
+    serial_number_auto = fields.Boolean(string='Auto-generate Serial Number', default=False)
 
     def action_open_numbering_master(self):
         return {
