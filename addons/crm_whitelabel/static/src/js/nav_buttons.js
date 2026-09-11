6f6fe312775481fceedbb869d04e556ad98b4f4d
crm-ux <crm@yantradynamics.in>
2026-09-11 13:58:12 +0530

    Update code for testing

--- addons/crm_whitelabel/static/src/js/nav_buttons.js
+++ addons/crm_whitelabel/static/src/js/nav_buttons.js
@@ -134,7 +134,6 @@ patch(ControlPanel.prototype, {
                     ...ctx,
                     from_total_pipeline: true,
                     default_stage_filter_val: val,
-                    create: false,
                 },
             }, { clearBreadcrumbs: true });
         } else {
