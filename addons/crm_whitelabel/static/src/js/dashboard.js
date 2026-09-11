4a9d4e267c652a4c1fd179ce19cd8daff12a7e6c
crm-ux <crm@yantradynamics.in>
2026-09-11 18:00:49 +0530

    Update code for testing

--- addons/crm_whitelabel/static/src/js/dashboard.js
+++ addons/crm_whitelabel/static/src/js/dashboard.js
@@ -83,7 +83,7 @@ class CrmDashboard extends Component {
             },
             target: 'current',
         });
-    },
+    }
 
     openServiceTicketSeries() {
         this.action.doAction({
