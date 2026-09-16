db05b5cf4de32962d92c3e6388157be2a32983a4
crm-ux <crm@yantradynamics.in>
2026-09-16 16:56:17 +0530

    Update code for testing

--- addons/custom_crm_extended/static/src/js/equipement_form.js
+++ addons/custom_crm_extended/static/src/js/equipement_form.js
@@ -96,7 +96,6 @@ export class EquipmentFormController extends FormController {
                         }, 50);
                     }
                 });
-                document.addEventListener("focusin", handleDateFocus);
             });
 
             onWillUnmount(() => {
