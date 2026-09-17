e29f54ba9895eba4b81b133004f9cd5c7c08ff0a
crm-ux <crm@yantradynamics.in>
2026-09-17 16:52:34 +0530

    Update code for testing

--- addons/custom_crm_extended/models/res_users.py
+++ addons/custom_crm_extended/models/res_users.py
@@ -41,37 +41,6 @@ class ResUsers(models.Model):
             self.with_context(skip_sync=True)._sync_employee_records(self)
         return res
     
-    def _sync_employee_records(self, users):
-        for user in users:
-            if user.share or not user.id:
-                continue
-
-            emp = self.env['hr.employee'].sudo().search([('user_id', '=', user.id)], limit=1)
-            # If no employee exists yet, do NOTHING until admin clicks "Create Employee"
-            if not emp:
-                continue
-
-            employee_name = (user.name or user.login or user.email or '').strip()
-            emp_vals = {
-                'name': employee_name,
-                'work_email': user.email or user.login,
-                'company_id': user.company_id.id if user.company_id else False,
-            }
-            if user.crm_department_id:
-                emp_vals['department_id'] = user.crm_department_id.id
-            if user.crm_job_id:
-                emp_vals['job_id'] = user.crm_job_id.id
-            if user.crm_manager_id:
-                mgr = self.env['hr.employee'].sudo().search([('user_id', '=', user.crm_manager_id.id)], limit=1)
-                if mgr:
-                    emp_vals['parent_id'] = mgr.id
-            if user.crm_expense_manager_id:
-                emp_vals['expense_manager_id'] = user.crm_expense_manager_id.id
-            if user.crm_employee_tag_ids:
-                emp_vals['category_ids'] = [(6, 0, user.crm_employee_tag_ids.ids)]
-            
-            emp.sudo().write(emp_vals)
-
     def _assign_default_groups(self, users):
         try:
             portal_group = self.env.ref('base.group_portal')
@@ -117,3 +86,59 @@ class ResUsers(models.Model):
         if employees:
             employees.unlink()
         return super(ResUsers, self).unlink()
+
+    def _sync_employee_records(self, users):
+        for user in users:
+            if user.share or not user.id:
+                continue
+
+            emp = self.env['hr.employee'].sudo().search([('user_id', '=', user.id)], limit=1)
+            # If no employee exists yet, do NOTHING until admin clicks "Create Employee"
+            if not emp:
+                continue
+
+            employee_name = (user.name or user.login or user.email or '').strip()
+            emp_vals = {
+                'name': employee_name,
+                'work_email': user.email or user.login,
+                'company_id': user.company_id.id if user.company_id else False,
+                'department_id': user.crm_department_id.id if user.crm_department_id else False,
+                'job_id': user.crm_job_id.id if user.crm_job_id else False,
+                'expense_manager_id': user.crm_expense_manager_id.id if user.crm_expense_manager_id else False,
+                'category_ids': [(6, 0, user.crm_employee_tag_ids.ids)] if user.crm_employee_tag_ids else [(5, 0, 0)],
+            }
+            if user.crm_manager_id:
+                mgr = self.env['hr.employee'].sudo().search([('user_id', '=', user.crm_manager_id.id)], limit=1)
+                emp_vals['parent_id'] = mgr.id if mgr else False
+            else:
+                emp_vals['parent_id'] = False
+            
+            emp.with_context(skip_sync=True).sudo().write(emp_vals)
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'
+
+    def write(self, vals):
+        res = super().write(vals)
+        if not self.env.context.get('skip_sync'):
+            self.with_context(skip_sync=True)._sync_user_records()
+        return res
+
+    def _sync_user_records(self):
+        for emp in self:
+            if not emp.user_id:
+                continue
+            user = emp.user_id.sudo()
+            user_vals = {
+                'crm_department_id': emp.department_id.id if emp.department_id else False,
+                'crm_job_id': emp.job_id.id if emp.job_id else False,
+                'crm_expense_manager_id': emp.expense_manager_id.id if emp.expense_manager_id else False,
+                'crm_employee_tag_ids': [(6, 0, emp.category_ids.ids)] if emp.category_ids else [(5, 0, 0)],
+            }
+            if emp.parent_id and emp.parent_id.user_id:
+                user_vals['crm_manager_id'] = emp.parent_id.user_id.id
+            else:
+                user_vals['crm_manager_id'] = False
+
+            user.with_context(skip_sync=True).write(user_vals)
+
