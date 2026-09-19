7ff8cc44cf4125ab45079d2753a5b9c16a5d8739
crm-ux <crm@yantradynamics.in>
2026-09-19 12:28:46 +0530

    Update code for testing

--- addons/custom_crm_extended/controllers/export_all_fields.py
+++ addons/custom_crm_extended/controllers/export_all_fields.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import json
+import re
 from odoo import http
 from odoo.addons.web.controllers.export import Export
-from odoo.http import content_disposition
+from odoo.http import content_disposition, request
 
 
 class CustomExport(Export):
@@ -10,13 +11,23 @@ class CustomExport(Export):
     @http.route('/web/export/xlsx', type='http', auth="user")
     def web_export_xlsx(self, data):
         response = super().web_export_xlsx(data)
-        
-        # Check if custom_filename was requested (Equipment Master, Service Ticket, AMC Contract)
+
         try:
-            params = json.loads(data)
-            custom_filename = params.get('custom_filename')
-            if custom_filename:
-                response.headers['Content-Disposition'] = content_disposition(custom_filename)
+            params = json.loads(data) if isinstance(data, str) else data
+            model = params.get('model')
+            
+            TARGET_FILENAMES = {
+                'equipment.master': 'Equipment Master.xlsx',
+                'service.ticket': 'Service Ticket.xlsx',
+                'amc.contract': 'AMC Contract.xlsx',
+            }
+
+            if model in TARGET_FILENAMES:
+                clean_name = TARGET_FILENAMES[model]
+                # Replace the header directly
+                new_disposition = content_disposition(clean_name)
+                response.headers['Content-Disposition'] = new_disposition
+                response.headers['content-disposition'] = new_disposition
         except Exception:
             pass
 
